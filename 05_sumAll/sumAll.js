a92439087e1f52beb91b73fe2c99df18f9249fd8
Nicholas Crane <nickcrusher30@gmail.com>
2022-09-21 21:52:14 -0400

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // Check input arguments
+    if (typeof(num1) !== 'number' || typeof(num2) !== 'number') return 'ERROR'; 
+    if (num1 < 0 || num2 < 0) return 'ERROR';
 
+    // Organize inputs
+    let big = num1;
+    let small = num2;
+    if (num2 > num1) { big = num2; small = num1; }
+
+    // Sum
+    let sum = 0;
+    for (let i = small; i <= big; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
