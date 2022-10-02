40a75cacd4ad60173a607addc7240ca369b65fd0
Nicholas Crane <nickcrusher30@gmail.com>
2022-10-02 12:55:07 -0400

    Complete exercises from Fundamentals Part 5

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson;
+    let oldestAge = 0;
+    people.forEach(person => {
+        let currentAge = getAge(person);
+        if (currentAge > oldestAge) {
+            oldestAge = currentAge;
+            oldestPerson = person;
+        } 
+    })
 
+    return oldestPerson;
 };
+
+const getAge = function(person) {
+    if (person.yearOfDeath) return person.yearOfDeath - person.yearOfBirth;
+    const yearOfDeath = new Date().getFullYear();
+    return yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
